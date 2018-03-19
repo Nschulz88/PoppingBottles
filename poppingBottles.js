47fccf0f580a363eb1d09c81e0ecb5161461685d
Nschulz88 <natalie.schulz@me.com>
2018-03-19 02:17:10 +0000

    mid work push, first recycle cycle works

--- poppingBottles.js
+++ poppingBottles.js
@@ -1,13 +1,31 @@
-var invested = 20;
+var invested = 10;
 
 var popCost = 2;
 var bottlesPerCap = 0.25;
 var bottlesPerBottle = 0.5;
-var bottlesReceived = 0;
+var bottleStart = invested / popCost; // 5
+var capsCount = invested / popCost;
+var totalBottles = 0;
 
 function bottlesBack(invested) {
-    bottlesReceived = invested / popCost;
-    console.log(bottlesReceived);
+    var restCountBottles = bottleStart % 2;  // 1
+    var redeemedBottles = bottleStart - restCountBottles;  // 4
+    var recycle = redeemedBottles * bottlesPerBottle;  // 2
+    capsCount += recycle;
+    totalBottles += recycle + bottleStart;
+
+    var newCount = recycle + restCountBottles;
+
+    console.log(newCount);
+    console.log(capsCount);
+
+
+    
+
+
+
+
+    console.log("Total bottles: " + totalBottles);
 }
 
 bottlesBack(invested);
