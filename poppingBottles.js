3bd944b63a6e27cdb9dc1873e0f38aa6792ca143
Nschulz88 <natalie.schulz@me.com>
2018-03-19 05:30:33 +0000

    Task 1 completed - working code

--- poppingBottles.js
+++ poppingBottles.js
@@ -3,8 +3,8 @@ var invested = 40;
 var popCost = 2;
 var capsPerBottle = 4;
 var bottlesPerBottle = 2;
-var bottleStart = invested / popCost; // 5
-var capsCount = invested / popCost;  // 5
+var bottleStart = invested / popCost;
+var capsCount = invested / popCost;
 var totalBottles = 0;
 var recycle = 0;
 var restCountBottles = 0;
@@ -13,23 +13,27 @@ var restCountCaps = 0;
 function bottlesBack(invested) {
     while (bottleStart >= 2 || capsCount >= 4){
     
-        var capsToRedeem = capsCount + restCountCaps;  // 5 + 0
-        restCountCaps = capsToRedeem % capsPerBottle; // 1
+        var capsToRedeem = capsCount;  
+        restCountCaps = capsToRedeem % capsPerBottle;
 
-        var capsAbleToRedeem = capsToRedeem - restCountCaps;  // 4
-        var bottlesFromCaps = capsAbleToRedeem / capsPerBottle; // 1
+        var capsAbleToRedeem = capsToRedeem - restCountCaps;
+        var bottlesFromCaps = capsAbleToRedeem / capsPerBottle;
+        // console.log("bottlesFromCaps: ", bottlesFromCaps);
 
         restCountBottles += bottlesFromCaps;
 
-        var bottlesToRedeem = bottleStart + restCountBottles;  // 5 + 1 
-        restCountBottles = bottlesToRedeem % bottlesPerBottle; // 1
+        var bottlesToRedeem = bottleStart + restCountBottles;
+        restCountBottles = bottlesToRedeem % bottlesPerBottle;
+        bottlesToRedeem = bottlesToRedeem - restCountBottles;
+
 
         recycle = bottlesToRedeem / bottlesPerBottle;
+        // console.log("Recycle: ", recycle);
         totalBottles += recycle + bottlesFromCaps;
         bottleStart = bottleStart - bottlesToRedeem + recycle + bottlesFromCaps;
         capsCount = capsCount - capsAbleToRedeem + bottlesFromCaps + recycle;
     }
-    console.log(restCountBottles);
+    // console.log(restCountBottles);
     console.log("Total bottles: ", totalBottles + (invested / popCost));
 }
 
