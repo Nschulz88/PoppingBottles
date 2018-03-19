ca3fa342a12338a918c023619f4044b29dffc465
Nschulz88 <natalie.schulz@me.com>
2018-03-19 05:08:32 +0000

    Very close to answer, just off my minor amount

--- poppingBottles.js
+++ poppingBottles.js
@@ -1,4 +1,4 @@
-var invested = 22;
+var invested = 40;
 
 var popCost = 2;
 var capsPerBottle = 4;
@@ -8,15 +8,26 @@ var capsCount = invested / popCost;  // 5
 var totalBottles = 0;
 var recycle = 0;
 var restCountBottles = 0;
+var restCountCaps = 0;
 
 function bottlesBack(invested) {
-    while (bottleStart >= 2){
-        var bottlesToRedeem = bottleStart + restCountBottles;
-        restCountBottles = bottlesToRedeem % bottlesPerBottle;
-        var i2 = bottlesToRedeem - restCountBottles;
-        recycle = i2 / bottlesPerBottle;
-        totalBottles += recycle;
-        bottleStart = bottleStart - i2 + recycle;
+    while (bottleStart >= 2 || capsCount >= 4){
+    
+        var capsToRedeem = capsCount + restCountCaps;  // 5 + 0
+        restCountCaps = capsToRedeem % capsPerBottle; // 1
+
+        var capsAbleToRedeem = capsToRedeem - restCountCaps;  // 4
+        var bottlesFromCaps = capsAbleToRedeem / capsPerBottle; // 1
+
+        restCountBottles += bottlesFromCaps;
+
+        var bottlesToRedeem = bottleStart + restCountBottles;  // 5 + 1 
+        restCountBottles = bottlesToRedeem % bottlesPerBottle; // 1
+
+        recycle = bottlesToRedeem / bottlesPerBottle;
+        totalBottles += recycle + bottlesFromCaps;
+        bottleStart = bottleStart - bottlesToRedeem + recycle + bottlesFromCaps;
+        capsCount = capsCount - capsAbleToRedeem + bottlesFromCaps + recycle;
     }
     console.log(restCountBottles);
     console.log("Total bottles: ", totalBottles + (invested / popCost));
